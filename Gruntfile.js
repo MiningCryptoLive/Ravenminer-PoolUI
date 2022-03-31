6f413e12537524c557695fcd3c45c0e8ac36e8e8
MiningCryptoLive <celldr69@gmail.com>
2022-03-31 09:25:14 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
             // the server port
             // can also be written as a function, e.g.
             // port: function() { return 8282; }
-            port: 8002,
+            port: 2222,
 
             // the host ip address
             // If specified to, for example, "127.0.0.1" the server will
